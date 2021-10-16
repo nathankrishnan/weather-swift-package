db5d1702c01127a8a9897cb896ba6ec0fa140d0c
Nathan Krishnan <3166572+nathankrishnan@users.noreply.github.com>
2021-10-15 19:09:38 -0700

    Update testGetLocationId() and implement testGetCurrentConditions()

--- Tests/WeatherTests/WeatherTests.swift
+++ Tests/WeatherTests/WeatherTests.swift
@@ -3,12 +3,34 @@ import XCTest
 
 final class WeatherTests: XCTestCase {
     let weather = Weather(forCity: "seattle")
-        
+
     func testGetLocationId() throws {
         let expectation = expectation(description: "Get Location Id")
         
-        if let result = try weather.getLocationId() {
-            print("Location Id: \(result)")
+        if let id = try weather.getLocationId() {
+            print("LOCATION ID: \(id)")
+            
+            XCTAssertTrue((type(of: id)) == Int.self)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testGetCurrentConditions() throws {
+        let expectation = expectation(description: "Get Current Weather Conditions")
+        
+        if let result = try weather.getCurrentConditions() {
+            let temp = result["temperature"]!
+            let condition = result["condition"]!
+            
+            print("TEMP: \(temp)")
+            print("CONDITION: \(condition)")
+            
+            XCTAssertTrue((type(of: temp)) == String.self)
+            XCTAssertTrue((type(of: condition)) == String.self)
+            
             expectation.fulfill()
         }
         
