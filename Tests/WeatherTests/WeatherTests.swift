34bfc780973b524e6d76c7e71d5fe7aba0191871
Nathan Krishnan <3166572+nathankrishnan@users.noreply.github.com>
2021-10-17 00:55:28 -0700

    Add test for creating a new Weather instance

--- Tests/WeatherTests/WeatherTests.swift
+++ Tests/WeatherTests/WeatherTests.swift
@@ -3,13 +3,31 @@ import XCTest
 
 final class WeatherTests: XCTestCase {
     let weather = Weather(forCity: "San Francisco")
+    
+    func testCreateWeatherInstance() throws {
+        let expectation = expectation(description: "Create a Weather instance")
+        let weather = Weather(forCity: "New York")
+        
+        print("URL ENCODED NAME: \(weather.city)")
+        XCTAssertTrue((type(of: weather.city)) == String.self)
+        if let condition = weather.condition, let temp = weather.temperature {
+            print("TEMP: \(temp)")
+            print("CONDITION: \(condition)")
+            
+            XCTAssertTrue((type(of: temp)) == String.self)
+            XCTAssertTrue((type(of: condition)) == String.self)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 
     func testGetLocationId() throws {
         let expectation = expectation(description: "Get Location Id")
-        print("CITY: \(weather.city)")
         
         if let id = try weather.getLocationId() {
-            print("NAME: \(weather.city)")
+            print("URL ENCODED NAME: \(weather.city)")
             print("LOCATION ID: \(id)")
             
             XCTAssertTrue((type(of: id)) == Int.self)
